f20284690bcd4b55f96d4303d4d69bcff97d0c71
Angelos Eleftheriou <elefthea@tcd.ie>
2026-01-22 10:37:00 +0000

    progress, kinda, doing validation layers now

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan_raii.hpp>
+#define GLFW_INCLUDE_VULKAN
 #include <memory>
+#include <ranges>
+/**
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #	include <vulkan/vulkan_raii.hpp>
 #else
 import vulkan_hpp;
 #endif
-#include <GLFW/glfw3.h>
+**/
 
+#include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
 const uint32_t WIDTH  = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<char const*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
 
 class HelloTriangleApplication
 {
@@ -26,21 +42,96 @@ class HelloTriangleApplication
 
   private:
 	GLFWwindow *window = nullptr;
+	vk::raii::Context  context;
+	vk::raii::Instance instance = nullptr;
 
 	void initWindow()
 	{
-		glfwInit();
+		// Set platform to Wayland if available
+		if (glfwPlatformSupported(GLFW_PLATFORM_WAYLAND))
+			glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
+		else if (glfwPlatformSupported(GLFW_PLATFORM_X11))
+			glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+			
+		if (!glfwInit())
+			throw std::runtime_error("Failed to initialize GLFW");
 
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		if (!window)
+			throw std::runtime_error("Failed to create GLFW window");
+			
+		glfwShowWindow(window);
 	}
 
 	void initVulkan()
 	{
+		createInstance();
 	}
+	void createInstance()
+	{
+		constexpr vk::ApplicationInfo appInfo{ .pApplicationName   = "Hello Triangle",
+			.applicationVersion = VK_MAKE_VERSION( 1, 0, 0 ),
+			.pEngineName        = "No Engine",
+			.engineVersion      = VK_MAKE_VERSION( 1, 0, 0 ),
+			.apiVersion         = vk::ApiVersion14 };
 
+		// Get the required layers
+		std::vector<char const*> requiredLayers;
+		if (enableValidationLayers) {
+			requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+		}
+		// Check if the required layers are supported by the Vulkan implementation.
+		auto layerProperties = context.enumerateInstanceLayerProperties();
+		if (std::ranges::any_of(requiredLayers, [&layerProperties](auto const& requiredLayer) {
+			return std::ranges::none_of(layerProperties,
+									   [requiredLayer](auto const& layerProperty)
+									   { return strcmp(layerProperty.layerName, requiredLayer) == 0; });
+		}))
+		{
+			throw std::runtime_error("One or more required layers are not supported!");
+		}
+
+
+		// Get the required instance extensions from GLFW.
+		uint32_t glfwExtensionCount = 0;
+		auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		auto extensionProperties = context.enumerateInstanceExtensionProperties();
+		for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+		{
+			bool found = false;
+			for (const auto& extensionProperty : extensionProperties)
+			{
+				if (strcmp(extensionProperty.extensionName, glfwExtensions[i]) == 0)
+				{
+					found = true;
+					break;
+				}
+			}
+			if (!found)
+			{
+				throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+			}
+		}
+
+		vk::InstanceCreateInfo createInfo{
+			.pApplicationInfo = &appInfo,
+			.enabledExtensionCount = glfwExtensionCount,
+			.ppEnabledExtensionNames = glfwExtensions};
+		/*
+		vk::InstanceCreateInfo createInfo{
+			.pApplicationInfo        = &appInfo,
+			.enabledLayerCount       = static_cast<uint32_t>(requiredLayers.size()),
+			.ppEnabledLayerNames     = requiredLayers.data(),
+			.enabledExtensionCount   = 0,
+			.ppEnabledExtensionNames = nullptr };
+		*/
+		instance = vk::raii::Instance(context, createInfo);
+		printf("extensions: %i\n", glfwExtensionCount);
+	}
 	void mainLoop()
 	{
 		while (!glfwWindowShouldClose(window))
@@ -69,6 +160,5 @@ int main()
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
-
 	return EXIT_SUCCESS;
 }
