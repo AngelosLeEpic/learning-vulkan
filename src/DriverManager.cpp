bf7385c85fca21b85f25b65b6ca53e4c3f7b888d
Angelos Eleftheriou <elefthea@tcd.ie>
2026-01-26 19:11:59 +0000

    added presentation buffer

---
+++ src/DriverManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hydrogen on 1/23/26.
+//
+
+#include "DriverManager.h"
---
+++ src/DriverManager.h
@@ -0,0 +1,16 @@
+//
+// Created by hydrogen on 1/23/26.
+//
+
+#ifndef LEARNING_VULKAN_DRIVERMANAGER_H
+#define LEARNING_VULKAN_DRIVERMANAGER_H
+
+
+
+class DriverManager {
+
+};
+
+
+
+#endif //LEARNING_VULKAN_DRIVERMANAGER_H
